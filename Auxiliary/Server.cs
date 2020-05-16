cecceb71fd83458064a8a451a6c821e294864370
tinyAdapter <tinyadapter@outlook.com>
2020-05-16 21:15:18 +0800

    一些更新
    1. 整理ServerCode
    2. 允许推送内容1多行显示
    3. 修复推送内容1未绑定ViewModel的问题

--- Auxiliary/Server.cs
+++ Auxiliary/Server.cs
@@ -8,13 +8,16 @@ namespace Auxiliary
 {
     public static class Server
     {
+        public static readonly string IP_ADDRESS = "";
         public static readonly string PROJECT_ADDRESS = "https://github.com/CHKZL/DDTV2/releases/latest";
         public static class RequestCode
         {
-            public static readonly int GET_TOGGLE_PUSH_NOTIFICATION = 20009;
-            public static readonly int GET_PUSH_NOTIFICATION = 20010;
+            public static readonly int GET_NEW_MEMBER_LIST_CONTENT = 20001;
+            public static readonly int GET_PUSH_NOTIFICATION_1 = 20005;
+            public static readonly int GET_TOGGLE_DYNAMIC_NOTIFICATION = 20009;
+            public static readonly int GET_DYNAMIC_NOTIFICATION = 20010;
             public static readonly int GET_LATEST_VERSION_NUMBER = 20011;
-            public static readonly int GET_UPDATE_ANNOUNCEMENT = 20011;
+            public static readonly int GET_LATEST_VERSION_ANNOUNCEMENT = 20012;
         }
     }
 }
