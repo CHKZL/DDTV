e2ead76850ebaba3503e85a0ebe93eb0a00bbdb0
tinyAdapter <tinyadapter@outlook.com>
2020-05-17 01:57:01 +0800

    基于Pipeline模式的缓存系统
    实现位于Auxiliary\CacheSystem.cs，原有的DataCache全部迁移至新的缓存系统

---
+++ Auxiliary/CacheSystem.cs
@@ -0,0 +1,214 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auxiliary
+{
+    public class Pipeline<TIn, TOut>
+    {
+        private List<IPipe> _steps = new List<IPipe>();
+
+        public void AddStep(IPipe step)
+        {
+            _steps.Add(step);
+        }
+
+        public TOut Accept(TIn initialObject)
+        {
+            object input = initialObject;
+            object output = null;
+            foreach (var s in _steps)
+            {
+                output = s.Invoke(input);
+                input = output;
+            }
+            return (TOut)output;
+        }
+    }
+
+    public class CachedObject<T>
+    {
+        private IAPIRequest _apiRequest;
+        private Pipeline<IAPIRequest, T> _pipeline;
+
+        public bool IsCached { get => _value != null; }
+        public DateTime LastUpdateTime { get; protected set; }
+        public int TimeoutSec { get; set; }
+        public bool IsTimedOut { get => (DateTime.Now - LastUpdateTime).TotalSeconds > TimeoutSec; }
+
+        private T _value;
+
+        public T Get()
+        {
+            if (IsCached && !IsTimedOut) return _value;
+            else return ForceUpdate();
+        }
+
+        public T ForceUpdate()
+        {
+            var result = _pipeline.Accept(_apiRequest);
+            _value = result;
+            LastUpdateTime = DateTime.Now;
+            return result;
+        }
+
+        public CachedObject(IAPIRequest apiRequest, Pipeline<IAPIRequest, T> pipeline)
+        {
+            _apiRequest = apiRequest;
+            _pipeline = pipeline;
+            TimeoutSec = int.MaxValue;
+        }
+    }
+
+    public class CachedObjectCollection<T>
+    {
+        private Dictionary<string, CachedObject<T>> _objects = new Dictionary<string, CachedObject<T>>();
+
+        private IAPIRequestBuilder _builder;
+        private Pipeline<IAPIRequest, T> _pipeline;
+        private int _timeoutSec = int.MaxValue;
+
+        public T Get(string name)
+        {
+            if (_objects.ContainsKey(name))
+            {
+                return _objects[name].Get();
+            }
+            else
+            {
+                var obj = new CachedObject<T>(_builder.Build(name), _pipeline);
+                obj.TimeoutSec = _timeoutSec;
+                _objects.Add(name, obj);
+                DataCache.BilibiliApiCount++;
+                return obj.Get();
+            }
+        }
+
+        public CachedObjectCollection(IAPIRequestBuilder builder, Pipeline<IAPIRequest, T> pipeline)
+        {
+            _builder = builder;
+            _pipeline = pipeline;
+        }
+
+        public CachedObjectCollection(IAPIRequestBuilder builder, Pipeline<IAPIRequest, T> pipeline, int timeoutSec)
+        {
+            _builder = builder;
+            _pipeline = pipeline;
+            _timeoutSec = timeoutSec;
+        }
+    }
+
+    public interface IAPIRequestBuilder
+    {
+        IAPIRequest Build(string args);
+    }
+
+    public enum APIPlatforms
+    {
+        Bilibili,
+        Youtube
+    }
+
+    public interface IAPIRequest
+    {
+        APIPlatforms Platform { get; }
+        string Url { get; }
+        int ID { get; }
+        WebClient WebClient { get; }
+        string ExceptionString { get; }
+    }
+
+    public class APIRequest : IAPIRequest
+    {
+        public APIPlatforms Platform { get; set; }
+        public string Url { get; set; }
+        public int ID { get; set; }
+        public WebClient WebClient { get; set; }
+        public string ExceptionString { get; set; }
+    }
+
+    public class BiliAPIRequest : IAPIRequest
+    {
+        public APIPlatforms Platform { get; set; }
+        public string BaseUrl { get; set; }
+        public int ID 
+        { 
+            get
+            {
+                if (int.TryParse(Args.Values.FirstOrDefault(), out int result)) return result;
+                else return -1;
+            }
+        }
+        public Dictionary<string, string> Args { get;set; }
+        public string Url 
+        {
+            get
+            {
+                var result = BaseUrl;
+                if (BaseUrl.Contains("?") && Args.Count > 0) result += "&";
+                if (!BaseUrl.Contains("?") && Args.Count > 0) result += "?";
+                foreach (var arg in Args)
+                {
+                    result += arg.Key + "=" + arg.Value;
+                }
+                return result;
+            }
+        }
+        public WebClient WebClient { get; set; }
+        public string ExceptionString { get; set; }
+    }
+
+    public interface IPipe
+    {
+        object Invoke(object input);
+    }
+
+    public class APIRequestToResponseJObjectPipe : IPipe
+    {
+        public object Invoke(object input)
+        {
+            var request = (IAPIRequest)input;
+            byte[] responseBytes;
+            try
+            {
+                responseBytes = request.WebClient.DownloadData(request.Url);
+            }
+            catch (Exception e)
+            {
+                InfoLog.InfoPrintf(request.ID + request.ExceptionString + ": " + 
+                    e.Message, InfoLog.InfoClass.Debug);
+                return null;
+            }
+            var responseString = Encoding.UTF8.GetString(responseBytes);
+            var jsonObject = JObject.Parse(responseString);
+            return jsonObject;
+        }
+    }
+
+    public class APIRequestToResponseJObjectWithArgPipe : IPipe
+    {
+        public object Invoke(object input)
+        {
+            var request = (IAPIRequest)input;
+            byte[] responseBytes;
+            try
+            {
+                responseBytes = request.WebClient.DownloadData(request.Url);
+            }
+            catch (Exception e)
+            {
+                InfoLog.InfoPrintf(request.ID + request.ExceptionString + ": " +
+                    e.Message, InfoLog.InfoClass.Debug);
+                return null;
+            }
+            var responseString = Encoding.UTF8.GetString(responseBytes);
+            var jsonObject = JObject.Parse(responseString);
+            jsonObject.Add("@@", request.ID);
+            return jsonObject;
+        }
+    }
+}
--- Auxiliary/DataCache.cs
+++ Auxiliary/DataCache.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,14 +11,200 @@ namespace Auxiliary
     public class DataCache
     {
         public static int BilibiliApiCount = 0;
+    }
 
-        public static Dictionary<string, string> 通过UID获取房间号键值对=new Dictionary<string, string>();
+    public class GetRoomInfoOldAPIRequestBuilder : BiliAPIRequest, IAPIRequestBuilder
+    {
+        public GetRoomInfoOldAPIRequestBuilder()
+        {
+            BaseUrl = "https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld";
+            Platform = APIPlatforms.Bilibili;
+            ExceptionString = "获取房间信息（旧API）失败";
+        }
 
-        public static Dictionary<string, string> 获取真实房间号键值对=new Dictionary<string, string>();
+        public IAPIRequest Build(string args)
+        {
+            Args = new Dictionary<string, string>
+            {
+                { "mid", args }
+            };
 
-        public static Dictionary<string, string> 获取标题键值对 = new Dictionary<string, string>();
-        public static Dictionary<string, DateTime> 获取标题有效期 = new Dictionary<string, DateTime>();
+            return this;
+        }
+    }
+
+    public class GetRoomInfoAPIRequestBuilder : BiliAPIRequest, IAPIRequestBuilder
+    {
+        public GetRoomInfoAPIRequestBuilder()
+        {
+            BaseUrl = "https://api.live.bilibili.com/room/v1/Room/get_info";
+            Platform = APIPlatforms.Bilibili;
+            ExceptionString = "获取房间信息（旧API）失败";
+        }
+
+        public IAPIRequest Build(string args)
+        {
+            Args = new Dictionary<string, string>
+            {
+                { "id", args }
+            };
+
+            return this;
+        }
+    }
+
+    public class ResponseJObjectToRoomIDPipe : IPipe
+    {
+        public object Invoke(object input)
+        {
+            var jObject = (JObject)input;
+
+            var roomid = jObject["data"]["roomid"].ToString();
+            return roomid;
+        }
+    }
+
+    public class ResponseJObjectToRealRoomIDPipe : IPipe
+    {
+        public object Invoke(object input)
+        {
+            var jObject = (JObject)input;
+
+            var live_status = jObject["data"]["live_status"].ToString();
+            if (live_status != "1")
+            {
+                return "-1";
+            }
+            var roomid = jObject["data"]["room_id"].ToString();
+            return roomid;
+        }
+    }
+
+    public class ResponseJObjectToRoomTitlePipe : IPipe
+    {
+        public object Invoke(object input)
+        {
+            var jObject = (JObject)input;
+
+            string roomName = jObject["data"]["title"].ToString()
+                .Replace(" ", "")
+                .Replace("/", "")
+                .Replace("\\", "")
+                .Replace("\"", "")
+                .Replace(":", "")
+                .Replace("*", "")
+                .Replace("?", "")
+                .Replace("<", "")
+                .Replace(">", "")
+                .Replace("|", "")
+                .ToString();
+
+            InfoLog.InfoPrintf("根据RoomId获取到标题:" + roomName, InfoLog.InfoClass.Debug);
+
+            return roomName;
+        }
+    }
+
+    public class ResponseJObjectToRoomInfoObjectPipe : IPipe
+    {
+        public object Invoke(object input)
+        {
+            var result = (JObject)input;
+            var originalRoomId = result["@@"].ToString();
+
+            if (result["data"]["room_id"].ToString() != originalRoomId)
+            {
+                for (int i = 0; i < bilibili.RoomList.Count(); i++)
+                {
+                    if (bilibili.RoomList[i].房间号 == originalRoomId)
+                    {
+                        bilibili.RoomList[i].房间号 = result["data"]["room_id"].ToString();
+                        break;
+                    }
+                }
+            }
+
+            var roominfo = new RoomInit.RoomInfo
+            {
+                房间号 = result["data"]["room_id"].ToString(),
+                标题 = result["data"]["title"].ToString().Replace(" ", "").Replace("/", "").Replace("\\", "").Replace("\"", "").Replace(":", "").Replace("*", "").Replace("?", "").Replace("<", "").Replace(">", "").Replace("|", ""),
+                直播状态 = result["data"]["live_status"].ToString() == "1" ? true : false,
+                UID = result["data"]["uid"].ToString(),
+                直播开始时间 = result["data"]["live_time"].ToString(),
+                平台 = "bilibili"
+            };
+
+            InfoLog.InfoPrintf("获取到房间信息:" + roominfo.UID + " " + (roominfo.直播状态 ? "已开播" : "未开播") + " " + (roominfo.直播状态 ? "开播时间:" + roominfo.直播开始时间 : ""), InfoLog.InfoClass.Debug);
+
+            return roominfo;
+        }
+    }
+
+    public class BiliCache
+    {
+        public WebClient GeneralWebClient { get; private set; }
+
+        private static readonly Lazy<BiliCache> _lazy = new Lazy<BiliCache>(() =>
+        {
+            var instance = new BiliCache();
+            var wc = new WebClient();
+            instance.GeneralWebClient = wc;
+
+            wc.Headers.Add("Accept: */*");
+            wc.Headers.Add("User-Agent: " + MMPU.UA.Ver.UA());
+            wc.Headers.Add("Accept-Language: zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4");
+            if (!string.IsNullOrEmpty(MMPU.Cookie))
+            {
+                wc.Headers.Add("Cookie", MMPU.Cookie);
+            }
+
+            var pipeline = new Pipeline<IAPIRequest, string>();
+            pipeline.AddStep(new APIRequestToResponseJObjectPipe());
+            pipeline.AddStep(new ResponseJObjectToRoomIDPipe());
+            instance.UIDsToRoomIDs = new CachedObjectCollection<string>(
+                new GetRoomInfoOldAPIRequestBuilder
+                {
+                    WebClient = wc
+                }, pipeline);
+
+            pipeline = new Pipeline<IAPIRequest, string>();
+            pipeline.AddStep(new APIRequestToResponseJObjectPipe());
+            pipeline.AddStep(new ResponseJObjectToRoomTitlePipe());
+            instance.RoomIDsToTitles = new CachedObjectCollection<string>(
+                new GetRoomInfoAPIRequestBuilder
+                {
+                    WebClient = wc
+                }, pipeline, 3600);
+
+            pipeline = new Pipeline<IAPIRequest, string>();
+            pipeline.AddStep(new APIRequestToResponseJObjectPipe());
+            pipeline.AddStep(new ResponseJObjectToRealRoomIDPipe());
+            instance.RoomIDsToRealRoomIDs = new CachedObjectCollection<string>(
+                new GetRoomInfoAPIRequestBuilder
+                {
+                    WebClient = wc
+                }, pipeline);
+
+            var pipeline2 = new Pipeline<IAPIRequest, RoomInit.RoomInfo>();
+            pipeline2.AddStep(new APIRequestToResponseJObjectWithArgPipe());
+            pipeline2.AddStep(new ResponseJObjectToRoomInfoObjectPipe());
+            instance.RoomIDsToRoomInfos = new CachedObjectCollection<RoomInit.RoomInfo>(
+                new GetRoomInfoAPIRequestBuilder
+                {
+                    WebClient = wc
+                }, pipeline2, 0/*不缓存*/);
+
+            return instance;
+        });
+
+        public static BiliCache Instance { get { return _lazy.Value; } }
+
+        private BiliCache() { }
 
 
+        public CachedObjectCollection<string> UIDsToRoomIDs { get; private set; }
+        public CachedObjectCollection<string> RoomIDsToTitles { get; private set; }
+        public CachedObjectCollection<string> RoomIDsToRealRoomIDs { get; private set; }
+        public CachedObjectCollection<RoomInit.RoomInfo> RoomIDsToRoomInfos { get; private set; }
     }
 }
