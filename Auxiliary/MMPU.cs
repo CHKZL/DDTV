2f9acbe3d8c37502c3d76e1dd817f7f511883243
米 <kuanhzilin@gmail.com>
2020-05-25 16:38:50 +0800

    2.0.3.3a更新

--- Auxiliary/MMPU.cs
+++ Auxiliary/MMPU.cs
@@ -28,7 +28,7 @@ namespace Auxiliary
         public static string 直播缓存目录 = "";
         public static int 直播更新时间 = 60;
         public static string 下载储存目录 = "";
-        public static string 版本号 = "2.0.3.1a";
+        public static string 版本号 = "2.0.3.3a";
         public static string[] 不检测的版本号 = {};
         public static bool 第一次打开播放窗口 = true;
         public static int 默认音量 = 0;
