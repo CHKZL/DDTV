b198166d255f4da11797a338d354800621cd8374
米 <kuanhzilin@gmail.com>
2021-01-19 22:57:39 +0800

    修复弹幕加载所需的外部依赖文件

--- Auxiliary/MMPU.cs
+++ Auxiliary/MMPU.cs
@@ -35,9 +35,9 @@ namespace Auxiliary
         public static string 直播缓存目录 = "";
         public static int 直播更新时间 = 20;
         public static string 下载储存目录 = "";
-        public static string 版本号 = "2.0.5.0a";
+        public static string 版本号 = "2.0.5.0b";
         public static string 开发版本号 = $"开发模式(基于Ver{版本号}主分支)";     
-        public static string[] 不检测的版本号 = { "2.0.4.8.2021.b" };
+        public static string[] 不检测的版本号 = { };
         public static bool 第一次打开播放窗口 = true;
         public static int 默认音量 = 0;
         public static int 缩小功能 = 1;
