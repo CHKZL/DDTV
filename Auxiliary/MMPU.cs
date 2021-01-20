4f3e6537b42fb81fab4c8811bcbb38be73f44b70
米 <kuanhzilin@gmail.com>
2021-01-20 23:59:57 +0800

    增加高分辨率非100%下播放窗口的一键界面自动排序功能支持

--- Auxiliary/MMPU.cs
+++ Auxiliary/MMPU.cs
@@ -18,6 +18,7 @@ using Newtonsoft.Json.Linq;
 using System.Diagnostics.CodeAnalysis;
 using System.Windows;
 using static Auxiliary.bilibili;
+using System.Drawing;
 
 namespace Auxiliary
 { 
@@ -1502,7 +1503,29 @@ namespace Auxiliary
             else
                 return b;
         }
-
+        ///获取当前系统的dpi数值
+        public static void SystemDpi(out int x, out int y)
+        {
+            using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                x = (int)g.DpiX;
+                y = (int)g.DpiY;
+                g.Dispose();
+            }
+        }
+        ///根据当前系统dpi数值匹配 当前系统的桌面缩放比例
+        public static double Scaling(int DpiIndex)
+        {
+            switch (DpiIndex)
+            {
+                case 96: return 1;
+                case 120: return 1.25;
+                case 144: return 1.5;
+                case 168: return 1.75;
+                case 192: return 2;
+            }
+            return 1;
+        }
         public class UA
         {
             [SuppressMessage("ReSharper", "InconsistentNaming")]
