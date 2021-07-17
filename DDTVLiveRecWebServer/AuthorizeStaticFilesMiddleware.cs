39ac61c0d40285cffd167669094774242a21aacd
CHKZL <kuanhzilin@gmail.com>
2021-07-18 01:45:13 +0800

    增加了API相关的底层支持和加密验证相关操作；重构了WEB的API鉴权部分代码；增加了文件鉴权中间件；增加了ApiToken相关代码；增加了"system_info"API接口；

---
+++ DDTVLiveRecWebServer/AuthorizeStaticFilesMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDTVLiveRecWebServer
+{
+    public class AuthorizeStaticFilesMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public AuthorizeStaticFilesMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var providedApiKey = context.Request.Headers["FileSig"].FirstOrDefault();
+
+            //和API请求加密过程同理，在Headers里面附带FileSig，然后进行校验
+
+
+            /*
+                (施工中)文件鉴权操作逻辑部分
+             */ 
+
+            //如果鉴权失败，返回未经授权的提示
+            await context.ChallengeAsync();
+            //如果鉴权通过，但是文件权限不足或者不存在时返回ForbidAsync
+            await context.ForbidAsync();
+            //鉴权通过，并且文件存在，则返回文件流
+            await _next(context);
+        }
+    }
+}
