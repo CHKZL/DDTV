3fdabae2b10ea31f2d4746c18709e2ffa8af4deb
CHKZL <kuanhzilin@gmail.com>
2021-08-15 17:05:11 +0800

    统一所有内置时间戳为UTC；增加自动升级程序更新的文件路径检测；

--- DDTVLiveRecWebServer/API/WebLogin.cs
+++ DDTVLiveRecWebServer/API/WebLogin.cs
@@ -32,11 +32,12 @@ namespace DDTVLiveRecWebServer.API
             {
                 if(是否初次登陆)
                 {
-                    下次可登陆时间 = DateTime.Now.AddSeconds(5);
+                    下次可登陆时间 = DateTime.UtcNow;
+                    是否初次登陆 = false;
                 }
-                if (DateTime.Now < 下次可登陆时间)
+                if (DateTime.UtcNow.AddSeconds(3) > 下次可登陆时间)
                 {
-                    下次可登陆时间 = DateTime.Now.AddSeconds(5);
+                    下次可登陆时间 = DateTime.UtcNow.AddSeconds(5);
                     if(UserName==Auxiliary.MMPU.WebUserName&&Password==Auxiliary.MMPU.WebPassword)
                     {
                         错误次数 = 0;
@@ -53,24 +54,30 @@ namespace DDTVLiveRecWebServer.API
                     {
                         if(错误次数>=5)
                         {
-                            下次可登陆时间 = DateTime.Now.AddMinutes(30);
+                            下次可登陆时间 = DateTime.UtcNow.AddMinutes(30);
+                            return JsonConvert.SerializeObject(new Auxiliary.RequestMessage.ServerClass.Login()
+                            {
+                                message = "验证失败,服务器拒绝鉴权操作10分钟",
+                                result = false,
+                            });
                         }
                         else
                         {
                             错误次数++;
+                            return JsonConvert.SerializeObject(new Auxiliary.RequestMessage.ServerClass.Login()
+                            {
+                                message = "验证失败,还有" + (5 - 错误次数) + "次机会，全部错误将停止鉴权10分钟",
+                                result = false,
+                            });
                         }
-                        return JsonConvert.SerializeObject(new Auxiliary.RequestMessage.ServerClass.Login()
-                        {
-                            message = "验证失败,还有" + (5 - 错误次数) + "次机会，全部错误将停止鉴权10分钟",
-                            result = false,
-                        });
+                       
                     }
                 }
                 else
                 {
                     return JsonConvert.SerializeObject(new Auxiliary.RequestMessage.ServerClass.Login()
                     {
-                        message = "操作过于频繁",
+                        message = "操作过于频繁，请稍后再试",
                         result = false,
                     });
                 }
