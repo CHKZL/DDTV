ba3fb149727fc0e1aba4df65d2730635108478b3
hegugu-ng <2949434209@qq.com>
2021-08-18 15:20:05 +0800

    概览页面框架重写

--- WebUI/src/utils/request.js
+++ WebUI/src/utils/request.js
@@ -37,47 +37,47 @@ service.interceptors.response.use(response => {
   if (error && error.response) {
     // 1.公共错误处理
     // 2.根据响应码具体处理
-    switch (error.response.status) {
-      case 400:
-        error.message = '错误请求'
-        break;
-      case 401:
-        error.message = '未授权，请重新登录'
-        break;
-      case 403:
-        error.message = '拒绝访问'
-        break;
-      case 404:
-        error.message = '请求错误,未找到该资源'
-        window.location.href = "/NotFound"
-        break;
-      case 405:
-        error.message = '请求方法未允许'
-        break;
-      case 408:
-        error.message = '请求超时'
-        break;
-      case 500:
-        error.message = '服务器端出错'
-        break;
-      case 501:
-        error.message = '网络未实现'
-        break;
-      case 502:
-        error.message = '网络错误'
-        break;
-      case 503:
-        error.message = '服务不可用'
-        break;
-      case 504:
-        error.message = '网络超时'
-        break;
-      case 505:
-        error.message = 'http版本不支持该请求'
-        break;
-      default:
-        error.message = `连接错误${error.response.status}`
-    }
+    // switch (error.response.status) {
+    //   case 400:
+    //     error.message = '错误请求'
+    //     break;
+    //   case 401:
+    //     error.message = '未授权，请重新登录'
+    //     break;
+    //   case 403:
+    //     error.message = '拒绝访问'
+    //     break;
+    //   case 404:
+    //     error.message = '请求错误,未找到该资源'
+    //     window.location.href = "/NotFound"
+    //     break;
+    //   case 405:
+    //     error.message = '请求方法未允许'
+    //     break;
+    //   case 408:
+    //     error.message = '请求超时'
+    //     break;
+    //   case 500:
+    //     error.message = '服务器端出错'
+    //     break;
+    //   case 501:
+    //     error.message = '网络未实现'
+    //     break;
+    //   case 502:
+    //     error.message = '网络错误'
+    //     break;
+    //   case 503:
+    //     error.message = '服务不可用'
+    //     break;
+    //   case 504:
+    //     error.message = '网络超时'
+    //     break;
+    //   case 505:
+    //     error.message = 'http版本不支持该请求'
+    //     break;
+    //   default:
+    //     error.message = `连接错误${error.response.status}`
+    // }
   } else {
     // 超时处理
     if (JSON.stringify(error).includes('timeout')) {
