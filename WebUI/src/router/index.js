23fb8997df6ccd85a42efc860f4d09dd6b98c7f8
hegugu-ng <2949434209@qq.com>
2021-08-19 20:44:31 +0800

    时间戳问题先堵上 修复路由反复横跳的问题

--- WebUI/src/router/index.js
+++ WebUI/src/router/index.js
@@ -78,7 +78,6 @@ router.beforeEach((to, from, next) => {
 
   // 如果用户登录了还想要回到登录页 取消跳转
   if (to.name == 'Login' && isAuthenticated) next(false)
-  else next()
   // 动态添加 title 后面拼接一个名称
   document.title =  to.meta.title.concat(" - 你的地表最强B站录播机")
 
