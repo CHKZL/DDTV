b6cd4584676a51737b45c82e9ec5715a867d91b4
hegugu-ng <2949434209@qq.com>
2021-09-15 12:18:53 +0800

    更改

--- WebUI/public/static/config.js
+++ WebUI/public/static/config.js
@@ -2,5 +2,5 @@ window.apiObj = {
   // 如果您是前后端部署在一起，本配置填写 false 即可
   // 如果您是前端后端分离 这里配置你的后端地址
   // 如 http(s)://you.host:port (最后面不需要 / )
-    apiUrl: false,
+    apiUrl: "https://test.ddtv.pro:11419",
   }
