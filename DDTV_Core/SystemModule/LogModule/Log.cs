a24eb537c8ecbe8123856ad16cfd7cc59a866b26
米 <kuanhzilin@gmail.com>
2021-11-10 20:25:09 +0800

    增加日志和消息系统

---
+++ DDTV_Core/SystemModule/LogModule/Log.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace DDTV_Core.SystemModule.Log
+{
+    /// <summary>
+    /// 核心日志信息处理
+    /// </summary>
+    public class Log
+    {
+        private static int LogLevel = (int)LogClass.LogType.All;
+        private static readonly Stopwatch RunningTimeStopwatch = new();
+
+        public static void AddLog(string Source,LogClass.LogType logType,string Message)
+        {
+            Task.Run(() => 
+            {
+                LogClass logClass = new()
+                {
+                    RunningTime = RunningTimeStopwatch.Elapsed.Seconds,
+                    Message = Message,
+                    Source = Source,
+                    Time = DateTime.Now,
+                    Type = logType
+                };
+                if ((int)logType < LogLevel)
+                {
+                    Console.WriteLine($"{logClass.Time}:[{Enum.GetName(typeof(LogClass.LogType), (int)logType)}][{Source}]{Message}");
+                }
+               if (!LogDB.Operate.AddDb(logClass))
+                {
+                    ErrorLogFileWrite("LogSystem","日志数据库写入失败");
+                }
+            }).Start();
+        }
+        public static void ErrorLogFileWrite(string Source,string Message)
+        {
+            string ErrorText = $"{DateTime.Now}:[Error][{Source}][{RunningTimeStopwatch.Elapsed.Seconds}]{Message}！";
+            File.AppendAllText(LogDB.ErrorFilePath, ErrorText, Encoding.UTF8);
+        }
+        public static void LogInit(LogClass.LogType log = LogClass.LogType.All)
+        {
+            LogLevel= (int)log;
+            RunningTimeStopwatch.Start();
+        }
+    }
+}
---
+++ DDTV_Core/SystemModule/LogModule/LogClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTV_Core.SystemModule.Log
+{
+    public class LogClass
+    {
+        public enum LogType
+        {
+            Error = 10,//会造成整个系统无法运行的严重错误
+            Warn = 20,//会造成错误，但是不影响运行的警告
+            Info = 30,//系统一般消息
+            Debug = 40,//调试信息
+            Trace = 50,//一些追踪数据
+            All=999,//打开所有日志
+        }
+        public string? Message { set; get; }//日志内容
+        public LogType Type { set; get; }//日志类型
+        public DateTime Time { set; get; }//系统时间
+        public int RunningTime { set; get; }//软件的运行时间
+        public string? Source { set; get; }//来源
+    }
+}
