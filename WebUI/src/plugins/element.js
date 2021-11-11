351feae563d01f9e47806700ba98cfb5411d595a
米 <kuanhzilin@gmail.com>
2021-11-12 00:16:20 +0800

    清理历史垃圾

--- WebUI/src/plugins/element.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'element-ui/lib/theme-chalk/base.css'
-import 'element-ui/lib/theme-chalk/display.css'
-
-Vue.use(Element)
