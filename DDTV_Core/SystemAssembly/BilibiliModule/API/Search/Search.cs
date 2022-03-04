2cdcb0f343c9c31a6a403ded410af13bd55a1d2a
米 <kuanhzilin@gmail.com>
2022-03-04 23:53:34 +0800

    修改GUI发布弹幕为异步发送防止卡UI主线程；WEB版本融合B站搜索服务；增加“File_GetTypeFileList”接口；增加“User_Search”接口

---
+++ DDTV_Core/SystemAssembly/BilibiliModule/API/Search/Search.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDTV_Core.SystemAssembly.BilibiliModule.API.Search
+{
+    public class Search
+    {
+        public static JObject TypeSearch(string KeyWord)
+        {
+            string B = NetworkRequestModule.Get.Get.GetRequest("http://api.bilibili.com/x/web-interface/search/type?"+
+                $"search_type=live_user&" +
+                $"keyword={KeyWord}");
+            JObject JO = (JObject)JsonConvert.DeserializeObject(B);
+            return JO;
+        }
+    }
+}
