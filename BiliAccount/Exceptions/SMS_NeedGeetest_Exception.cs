7dfcf600c9f4756dc1e159fff44ef7ec2a6fe362
米 <kuanhzilin@gmail.com>
2022-05-15 15:49:25 +0800

    修改BiliAccount以支持.NET6，并且减少非win环境下的依赖

---
+++ BiliAccount/Exceptions/SMS_NeedGeetest_Exception.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BiliAccount.Exceptions
+{
+    /// <summary>
+    /// 发送短信需要极验验证时引发的错误
+    /// </summary>
+    public class SMS_NeedGeetest_Exception : Exception
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="recaptcha_url">校验弹窗地址</param>
+        public SMS_NeedGeetest_Exception(string recaptcha_url) : base("需要极验校验！")
+        {
+            url = recaptcha_url;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// 校验弹窗地址
+        /// </summary>
+        public string url { get; private set; }
+
+        #endregion Public Properties
+    }
+}
