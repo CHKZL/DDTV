63ce62eecd35978bcbdf39bea4043bc5bb00e8d2
米 <kuanhzilin@gmail.com>
2022-08-30 01:02:29 +0800

    Ver3.2.16.765累积更新

--- DDTV_Core/InitDDTV_Core.cs
+++ DDTV_Core/InitDDTV_Core.cs
@@ -31,7 +31,7 @@ namespace DDTV_Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string ClientAID = string.Empty;
         public static SatrtType InitType = SatrtType.DDTV_Core;
-        public static string CompiledVersion = "2022-08-29 14:24:15";
+        public static string CompiledVersion = "2022-08-30 00:56:45";
 
         /// <summary>
         /// 初始化COre
