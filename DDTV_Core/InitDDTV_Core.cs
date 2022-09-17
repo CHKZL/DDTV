5c19306d179649905767f63c6d4fd151b16603e0
米 <kuanhzilin@gmail.com>
2022-09-17 17:04:36 +0800

    推送Dev更新；补充：上个版本中增加了“有全屏程序时不提示更新”的设定

--- DDTV_Core/InitDDTV_Core.cs
+++ DDTV_Core/InitDDTV_Core.cs
@@ -31,7 +31,7 @@ namespace DDTV_Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string ClientAID = string.Empty;
         public static SatrtType InitType = SatrtType.DDTV_Core;
-        public static string CompiledVersion = "2022-09-03 23:38:32";
+        public static string CompiledVersion = "2022-09-17 17:02:33";
 
         /// <summary>
         /// 初始化COre
