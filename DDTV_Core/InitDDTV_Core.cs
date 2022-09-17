7470f207df78d7559ff99c4fbf887302ddbd9211
米 <kuanhzilin@gmail.com>
2022-09-18 03:31:35 +0800

    修复添加监控失败的问题；’修复文件夹名称预设解析错误的问题；

--- DDTV_Core/InitDDTV_Core.cs
+++ DDTV_Core/InitDDTV_Core.cs
@@ -31,7 +31,7 @@ namespace DDTV_Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string ClientAID = string.Empty;
         public static SatrtType InitType = SatrtType.DDTV_Core;
-        public static string CompiledVersion = "2022-09-18 02:51:59";
+        public static string CompiledVersion = "2022-09-18 03:30:35";
 
         /// <summary>
         /// 初始化COre
