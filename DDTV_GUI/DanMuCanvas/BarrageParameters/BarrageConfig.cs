4ccf381c6fe9f03bbde2d22b448915870212e856
米 <kuanhzilin@gmail.com>
2022-12-04 00:48:51 +0800

    新增弹幕发送UI，优化弹幕发送体验；增加单独的弹幕浏览窗口；优化弹幕渲染逻辑；增加野生字幕显示功能

--- DDTV_GUI/DanMuCanvas/BarrageParameters/BarrageConfig.cs
+++ DDTV_GUI/DanMuCanvas/BarrageParameters/BarrageConfig.cs
@@ -14,16 +14,6 @@ namespace DDTV_GUI.DanMuCanvas.BarrageParameters
     {
         #region 相关数据
         /// <summary>
-        /// 每行的起始高度
-        /// </summary>
-        public List<decimal> heightList;
-        public List<Color> colorList;
-        public Random random;
-        /// <summary>
-        /// 弹幕行数
-        /// </summary>
-        public int rowCount;
-        /// <summary>
         /// 每次循环时字幕的显示（减小lengthList）的速度
         /// </summary>
         public decimal reduceSpeed;
@@ -32,59 +22,22 @@ namespace DDTV_GUI.DanMuCanvas.BarrageParameters
         /// </summary>
         public double initFinishTime;
         /// <summary>
-        /// 弹幕总高度
+        /// 弹幕当前高度
         /// </summary>
-        public int height;
-        #region 运行时
-        /// <summary>
-        /// 每行的弹幕未显示长度
-        /// </summary>
-        private List<decimal> lengthList;
-        /// <summary>
-        /// 存储当前没有展现的弹幕
-        /// </summary>
-        private List<MessageInformation> messages;
-        /// <summary>
-        /// 是否是开发者模式(如果是开发者模式则会自动产生弹幕)
-        /// </summary>
-        private bool isDevelopMode;
-        private Canvas canvas;
+        public int height=0;
         #endregion
+
+        #region 运行时
+        private Canvas canvas;
         #endregion
 
         #region 初始化
         public BarrageConfig(Canvas canvas)
         {
-            InitializeColors();
+            //InitializeColors();
             this.canvas = canvas;
-            random = new Random();
-            rowCount = 10;
             reduceSpeed = decimal.Parse("0.5");
             initFinishTime = double.Parse("10");
-            height = int.Parse("500");
-
-        }
-
-        private void CaculateHeightList(double canvasActualHeight)
-        {
-            heightList = new List<decimal>();
-            decimal actualHeight = decimal.Parse(canvasActualHeight.ToString());
-            heightList.Add(0);
-            //rowCount分之一高
-            decimal threeHeight = actualHeight / rowCount;
-            for (int i = 1; i < rowCount; i++)
-            {
-                heightList.Add(threeHeight * i);
-            }
-        }
-
-        /// <summary>
-        /// 初始化弹幕颜色
-        /// </summary>
-        private void InitializeColors()
-        {
-            colorList = new List<Color>();
-            colorList.Add(Color.FromRgb(0xFF, 0xFF, 0xFF));    
         }
 
         #endregion
@@ -106,6 +59,13 @@ namespace DDTV_GUI.DanMuCanvas.BarrageParameters
                                                        //double randomspeed = random.NextDouble();
                                                        //设置完成动画的时间
                                                        //实例化TextBlock和设置基本属性,并添加到Canvas中
+
+            inittop = height;
+            height += DDTV_Core.SystemAssembly.ConfigModule.CoreConfig.DanMuFontSize;
+            if(height>Max)
+            {
+                height = 0;
+            }
             TextBlock textblock = new TextBlock();
             
             //加上昵称显示
@@ -121,6 +81,7 @@ namespace DDTV_GUI.DanMuCanvas.BarrageParameters
             textblock.FontWeight = System.Windows.FontWeights.Bold;
             if (IsSubtitle)
             {
+
                 byte R = Convert.ToByte(DDTV_Core.SystemAssembly.ConfigModule.CoreConfig.SubtitleColor.Split(',')[0], 16);
                 byte G = Convert.ToByte(DDTV_Core.SystemAssembly.ConfigModule.CoreConfig.SubtitleColor.Split(',')[1], 16);
                 byte B = Convert.ToByte(DDTV_Core.SystemAssembly.ConfigModule.CoreConfig.SubtitleColor.Split(',')[2], 16);
@@ -153,26 +114,7 @@ namespace DDTV_GUI.DanMuCanvas.BarrageParameters
             textblock.BeginAnimation(Canvas.LeftProperty, animation);
         }
 
-        /// <summary>
-        /// 颜色随机
-        /// </summary>
-        /// <returns></returns>
-        private Brush GetRandomColor()
-        {
-            int index = random.Next(colorList.Count);
-            return new SolidColorBrush(colorList[index]);
-        }
 
-        /// <summary>
-        /// 依次从第一行放置到第三行
-        /// </summary>
-        /// <param name="index"></param>
-        /// <returns></returns>
-        private double GetHeight(int index)
-        {
-            decimal target = heightList[index % rowCount];
-            return double.Parse(target.ToString());
-        }
         #endregion
     }
 }
