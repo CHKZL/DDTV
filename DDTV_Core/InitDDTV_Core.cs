b0afcd51e8273c77e253cfdeb402897724f906db
米 <kuanhzilin@gmail.com>
2022-12-10 00:55:33 +0800

    优化UI刷新逻辑，提升效率；升级UI框架为3.4.0

--- DDTV_Core/InitDDTV_Core.cs
+++ DDTV_Core/InitDDTV_Core.cs
@@ -23,7 +23,7 @@ namespace DDTV_Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string ClientAID = string.Empty;
         public static SatrtType InitType = SatrtType.DDTV_Core;
-        public static string CompiledVersion = "2022-12-09 18:49:17";
+        public static string CompiledVersion = "2022-12-10 00:54:48";
         public static bool WhetherInitializationIsComplet = false;//是否初始化完成
         public static string UpdateNotice = string.Empty;
         public static bool IsDevDebug = false;
