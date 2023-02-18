108f98dc1710dc7f2115c669df251433c101cef1
米 <kuanhzilin@gmail.com>
2023-02-19 01:46:34 +0800

    在GUI监控列表右键菜单中增加了搜索选项

--- DDTV_Core/InitDDTV_Core.cs
+++ DDTV_Core/InitDDTV_Core.cs
@@ -24,7 +24,7 @@ namespace DDTV_Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string ClientAID = string.Empty;
         public static SatrtType InitType = SatrtType.DDTV_Core;
-        public static string CompiledVersion = "2023-02-18 23:32:46";
+        public static string CompiledVersion = "2023-02-19 01:45:23";
         public static bool WhetherInitializationIsComplet = false;//是否初始化完成
         public static string UpdateNotice = string.Empty;
         public static bool IsDevDebug = false;
