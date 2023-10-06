99abb0afb2415460368cd7d79a58cc83f8e45856
CHKZL <kuanhzilin@gmail.com>
2023-10-06 14:07:02 +0800

    给GUI增加一键添加关注列表全部直播间的功能

--- DDTV_Core/InitDDTV_Core.cs
+++ DDTV_Core/InitDDTV_Core.cs
@@ -26,7 +26,7 @@ namespace DDTV_Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string ClientAID = string.Empty;
         public static SatrtType InitType = SatrtType.DDTV_Core;
-        public static string CompiledVersion = "2023-10-05 23:22:26";
+        public static string CompiledVersion = "2023-10-06 14:06:42";
         public static bool WhetherInitializationIsComplet = false;//是否初始化完成
         public static string UpdateNotice = string.Empty;
         public static bool IsDevDebug = false;
