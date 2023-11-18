33c9f46f6a0b71d9be4f69cf723b2a20135e3246
米 <kuanhzilin@gmail.com>
2023-11-19 05:10:56 +0800

    消息系统和日志系统重构完成

--- Core/Init.cs
+++ Core/Init.cs
@@ -8,6 +8,14 @@ namespace Core
 {
     public class Init
     {
-        
+        public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public static string InitType = "DDTV";
+        public static string ClientAID = string.Empty;
+        public static string CompiledVersion = "2023-11-03 21:01:27";
+        public static bool IsDevDebug = false;
+        public static void Start(string InitType = "DDTV", string ClientAID = "", bool IsDev = false)
+        {
+            LogModule.Log.LogInit();
+        }
     }
 }
---
+++ Core/Tools/ColorConsole/ConsoleWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Tools.ColorConsole
+{
+     internal sealed class ConsoleWrapper : IConsoleWrapper
+    {
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public ConsoleColor ForegroundColor
+        {
+            get
+            {
+                return Console.ForegroundColor;
+            }
+            set
+            {
+                Console.ForegroundColor = value;
+            }
+        }
+
+        public ConsoleColor BackgroundColor
+        {
+            get
+            {
+                return Console.BackgroundColor;
+            }
+            set
+            {
+                Console.BackgroundColor = value;
+            }
+        }
+    }
+}
