492a8aacfba2f03647090f89e7ecd25af2946349
米 <kuanhzilin@gmail.com>
2024-01-08 21:52:18 +0800

    update

--- Core/Init.cs
+++ Core/Init.cs
@@ -13,7 +13,7 @@ namespace Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string InitType = "DDTV";
         public static string ClientAID = string.Empty;
-        public static string CompiledVersion = "2024-01-08 21:21:17";
+        public static string CompiledVersion = "2024-01-08 21:21:18";
         public static bool IsDevDebug = false;
         public static void Start()
         {
