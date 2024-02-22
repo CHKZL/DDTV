18a53ee4eca6e1b8905d4de582ec8fd3e3688189
米 <kuanhzilin@gmail.com>
2024-02-23 02:11:16 +0800

    编译时间硬编码自动化

--- Core/Init.cs
+++ Core/Init.cs
@@ -13,7 +13,7 @@ namespace Core
         public static string Ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + "-" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string InitType = "DDTV";
         public static string ClientAID = string.Empty;
-        public static string CompiledVersion = "2024-02-23 00:49:59";
+        public static string CompiledVersion = "CompilationTime";
         public static bool IsDevDebug = false;
         public static void Start()
         {
