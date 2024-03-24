68e3352eb2309b1b33b1afbb2cc6e87bdd9d3fb8
米 <kuanhzilin@gmail.com>
2024-03-25 02:21:38 +0800

    优化ws推送的JSON内容，隐藏没必要的数据，减少数据量

--- CLI/WebAppServices/WS/WebSocketQueue.cs
+++ CLI/WebAppServices/WS/WebSocketQueue.cs
@@ -1,11 +1,9 @@
 ﻿using Core.RuntimeObject;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace CLI.WebAppServices.WS
@@ -28,7 +26,7 @@ namespace CLI.WebAppServices.WS
         private static void OperationQueue_AddOperationRecord(object? sender, EventArgs e)
         {
             Core.LogModule.OperationQueue.pack<RoomCardClass> pack = (Core.LogModule.OperationQueue.pack<RoomCardClass>)sender;
-            string MessagePack = JsonSerializer.Serialize(pack, new JsonSerializerOptions() { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)});
+            string MessagePack = JsonConvert.SerializeObject(pack);
             MessageBase.WS_Send(MessagePack);
         }
     }
