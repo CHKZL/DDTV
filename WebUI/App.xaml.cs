41c50f471ddf9b4495b91f81cd444d99a6f98e33
CHKZL <kuanhzilin@gmail.com>
2024-04-10 12:40:24 +0800

    优化webui的结构，修正应用依赖

--- WebUI/App.xaml.cs
+++ WebUI/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace GUI
+namespace WebUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- WebUI/MainWindow.xaml.cs
+++ WebUI/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GUI
+namespace WebUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
