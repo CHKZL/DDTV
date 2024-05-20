98b2de17abdd04a3abe883b477563d84f8048c56
米 <kuanhzilin@gmail.com>
2024-05-21 00:56:17 +0800

    增加全局主窗口Snackbar提示框

--- Desktop/App.xaml.cs
+++ Desktop/App.xaml.cs
@@ -7,6 +7,10 @@ using Server;
 using Core.LogModule;
 using Core;
 using static Server.Program;
+using static Core.Network.Methods.User.UserInfo;
+using Wpf.Ui;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Desktop
 {
@@ -15,19 +19,26 @@ namespace Desktop
     /// </summary>
     public partial class App : Application
     {
+        public static ServiceProvider _ServiceProvider;
+
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            var services = new ServiceCollection();
+            services.AddSingleton<ISnackbarService, SnackbarService>();
+            _ServiceProvider = services.BuildServiceProvider();
             Host.CreateDefaultBuilder()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     Task.Run(() => Service.CreateHostBuilder(new string[] { "Desktop" }).Build().Run());
                     webBuilder.UseStartup<Server.Startup>();
-                    string rurl = $"http://0.0.0.0:{Config.Web._Port}";
+                    string rurl = $"http://{Config.Web._IP}:{Config.Web._Port}";
                     webBuilder.UseUrls(rurl);
                     Log.Info(nameof(Application), $"WebApplication开始运行，开始监听[{rurl}]");
                     Log.Info(nameof(Application), $"本地访问请浏览器打开[ http://127.0.0.1:{Config.Web._Port} ]");
                 }).Build().RunAsync();
+           
         }
     }
 
--- Desktop/Views/Pages/SettingsPage.xaml.cs
+++ Desktop/Views/Pages/SettingsPage.xaml.cs
@@ -4,11 +4,16 @@
 // All Rights Reserved.
 
 using Core;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows;
+using System.Windows.Navigation;
+using Wpf.Ui;
 using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Extensions;
 
 namespace Desktop.Views.Pages;
 
@@ -17,36 +22,10 @@ namespace Desktop.Views.Pages;
 /// </summary>
 public partial class SettingsPage
 {
+    
     public SettingsPage()
     {
         InitializeComponent();
-
-        //AppVersionTextBlock.Text = $"WPF UI - Simple Demo - {GetAssemblyVersion()}";
-
-        //if (ApplicationThemeManager.GetAppTheme() == ApplicationTheme.Dark)
-        //{
-        //    DarkThemeRadioButton.IsChecked = true;
-        //}
-        //else
-        //{
-        //    LightThemeRadioButton.IsChecked = true;
-        //}
-    }
-
-    private void OnLightThemeRadioButtonChecked(object sender, RoutedEventArgs e)
-    {
-        ApplicationThemeManager.Apply(ApplicationTheme.Light);
-    }
-
-    private void OnDarkThemeRadioButtonChecked(object sender, RoutedEventArgs e)
-    {
-        ApplicationThemeManager.Apply(ApplicationTheme.Dark);
-    }
-
-    private string GetAssemblyVersion()
-    {
-        return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString()
-            ?? String.Empty;
     }
 
     /// <summary>
@@ -57,5 +36,10 @@ public partial class SettingsPage
     private void OpenRecordingFolderInExplorer_Click(object sender, RoutedEventArgs e)
     {
         Process.Start("explorer.exe", Path.GetFullPath(Config.Core._RecFileDirectory));
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        MainWindow.SnackbarService.Show("保存设置", "保存设置成功", ControlAppearance.Success, null, TimeSpan.FromSeconds(2));
     }
 }
