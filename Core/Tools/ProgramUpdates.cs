76154e2fe7ad73dd0e1faa6d1c1906c0a837eec6
米 <kuanhzilin@gmail.com>
2024-06-09 21:56:03 +0800

    优化自动更新程序和本地的自动更新程序更新逻辑

--- Core/Tools/ProgramUpdates.cs
+++ Core/Tools/ProgramUpdates.cs
@@ -79,7 +79,8 @@ namespace Core.Tools
                         Version After = new Version(R_Ver);
                         if (After > Before)
                         {
-                            Update_UpdateProgram.Main(["CheckForUpdatedPrograms", (Core.Config.Core_RunConfig._DevelopmentVersion ? "dev" : "release")]);
+                            Update_UpdateProgram update_UpdateProgram = new();
+                            update_UpdateProgram.Main(["CheckForUpdatedPrograms", (Core.Config.Core_RunConfig._DevelopmentVersion ? "dev" : "release")]);
 
                             if (!Manual)
                                 NewVersionAvailableEvent?.Invoke(R_Ver, new EventArgs());
@@ -190,8 +191,8 @@ namespace Core.Tools
             public static bool Isdev = false;
 
             public static bool Update_For_UpdateProgram = false;
-            public static HttpClient _httpClient = new HttpClient();
-            public static void Main(string[] args)
+            
+            public void Main(string[] args)
             {
                 if (args.Length != 0)
                 {
@@ -209,11 +210,11 @@ namespace Core.Tools
                     }
                 }
 
-                Console.WriteLine("开始更新DDTV");
+                Log.Info(nameof(Update_UpdateProgram),"开始更新DDTV_Update");
                 Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;//将当前路径从 引用路径 修改至 程序所在目录
-                Console.WriteLine($"当前工作路径:{Environment.CurrentDirectory}");
-                Console.WriteLine(Environment.CurrentDirectory);
+                Log.Info(nameof(Update_UpdateProgram),$"当前工作路径:{Environment.CurrentDirectory}");
                 Dictionary<string, (string Name, string FilePath, long Size)> map = new Dictionary<string, (string Name, string FilePath, long Size)>();
+                HttpClient _httpClient = new HttpClient();
                 _httpClient.Timeout = new TimeSpan(0, 0, 10);
                 _httpClient.DefaultRequestHeaders.Referrer = new Uri("https://update5.ddtv.pro");
                 if (checkVersion())
@@ -252,7 +253,7 @@ namespace Core.Tools
                         int i = 1;
                         foreach (var item in map)
                         {
-                            Console.Write($"进度：{i}/{map.Count}  |  文件大小{item.Value.Size}字节，开始更新文件【{item.Value.Name}】.......");
+                            Log.Info(nameof(Update_UpdateProgram),$"进度：{i}/{map.Count}  |  文件大小{item.Value.Size}字节，开始更新文件【{item.Value.Name}】.......");
                             string directoryPath = Path.GetDirectoryName(item.Value.FilePath);
                             if (!Directory.Exists(directoryPath))
                             {
@@ -263,14 +264,14 @@ namespace Core.Tools
                             {
                                 dl_ok = DownloadFileAsync(item.Key, item.Value.FilePath);
                             } while (!dl_ok);
-                            Console.WriteLine($" | 更新文件【{item.Value.Name}】成功");
+                            Log.Info(nameof(Update_UpdateProgram),$" | 更新文件【{item.Value.Name}】成功");
                             i++;
                         }
-                        Console.WriteLine($"更新完成：更新DDTV到{type}-{R_ver}成功，请手动启动DDTV");
+                        Log.Info(nameof(Update_UpdateProgram),$"更新完成：更新Update程序完成");
                     }
                     else
                     {
-                        Console.WriteLine($"更新失败：获取更新列表失败，请检查网络状态");
+                        Log.Info(nameof(Update_UpdateProgram),$"更新Update程序失败：获取更新列表失败，请检查网络状态");
                     }
                 }
                 if (!Update_For_UpdateProgram)
@@ -285,7 +286,7 @@ namespace Core.Tools
             {
                 if (!File.Exists(Update_For_UpdateProgram ? "./ver.ini" : verFile))
                 {
-                    Console.WriteLine("更新失败，没找到版本标识文件");
+                    Log.Info(nameof(Update_UpdateProgram),"更新失败，没找到版本标识文件");
                     return true;
                 }
                 string[] Ver = File.ReadAllLines(Update_For_UpdateProgram ? "./ver.ini" : verFile);
@@ -298,18 +299,18 @@ namespace Core.Tools
                 }
                 if (string.IsNullOrEmpty(type) || string.IsNullOrEmpty(ver))
                 {
-                    Console.WriteLine("更新失败，版本标识文件内容错数");
+                    Log.Info(nameof(Update_UpdateProgram),"更新失败，版本标识文件内容错数");
                     return true;
                 }
                 if (ver.ToLower().StartsWith("dev"))
                 {
                     Isdev = true;
                 }
-                Console.WriteLine($"当前本地版本{type}-{ver}[{(Isdev ? "dev" : "release")}]");
-                Console.WriteLine("开始获取最新版本号....");
+                Log.Info(nameof(Update_UpdateProgram),$"当前本地版本{type}-{ver}[{(Isdev ? "dev" : "release")}]");
+                Log.Info(nameof(Update_UpdateProgram),"开始获取最新版本号....");
                 string DL_VerFileUrl = $"/{type}/{(Isdev ? "dev" : "release")}/ver.ini";
                 string R_Ver = Get(DL_VerFileUrl).TrimEnd();
-                Console.WriteLine($"获取到当前服务端版本:{R_Ver}");
+                Log.Info(nameof(Update_UpdateProgram),$"获取到当前服务端版本:{R_Ver}");
 
                 if (!string.IsNullOrEmpty(R_Ver) && R_Ver.Split('.').Length > 0)
                 {
@@ -319,18 +320,18 @@ namespace Core.Tools
                     Version After = new Version(R_Ver.Replace("dev", "").Replace("release", ""));
                     if (After > Before)
                     {
-                        Console.WriteLine($"检测到新版本，获取远程文件树开始更新.......");
+                        Log.Info(nameof(Update_UpdateProgram),$"检测到新版本，获取远程文件树开始更新Update程序.......");
                         return true;
                     }
                     else
                     {
-                        Console.WriteLine($"当前已是最新版本....");
+                        Log.Info(nameof(Update_UpdateProgram),$"当前Update已是最新版本....");
                         return false;
                     }
                 }
                 else
                 {
-                    Console.WriteLine($"获取新版本失败，请检查网络和代理状况.......");
+                    Log.Info(nameof(Update_UpdateProgram),$"获取新版本失败，请检查网络和代理状况.......");
                     return false;
                 }
             }
@@ -354,17 +355,20 @@ namespace Core.Tools
                         {
                             if (error_count > 3)
                             {
-                                Console.WriteLine($"更新失败，网络错误过多，请检查网络状况或者代理设置后重试.....");
+                                Log.Info(nameof(Update_UpdateProgram),$"更新失败，网络错误过多，请检查网络状况或者代理设置后重试.....");
                                 return "";
                             }
-                            Console.WriteLine($"使用备用服务器进行重试.....");
+                            Log.Info(nameof(Update_UpdateProgram),$"使用备用服务器进行重试.....");
                             FileDownloadAddress = AlternativeDomainName + URL;
-                            Console.WriteLine($"从主服务器获取更新失败，尝试从备用服务器获取....");
+                            Log.Info(nameof(Update_UpdateProgram),$"从主服务器获取更新失败，尝试从备用服务器获取....");
                         }
                         else
                         {
                             FileDownloadAddress = MainDomainName + URL;
                         }
+                        HttpClient _httpClient = new HttpClient();
+                        _httpClient.Timeout = new TimeSpan(0, 0, 10);
+                        _httpClient.DefaultRequestHeaders.Referrer = new Uri("https://update5.ddtv.pro");
                         str = _httpClient.GetStringAsync(FileDownloadAddress).Result;
                         error_count++;
                     }
@@ -374,22 +378,21 @@ namespace Core.Tools
                         switch (webex.Status)
                         {
                             case WebExceptionStatus.Timeout:
-                                Console.WriteLine($"下载文件超时:{FileDownloadAddress}");
+                                Log.Info(nameof(Update_UpdateProgram),$"下载文件超时:{FileDownloadAddress}");
                                 break;
 
                             default:
-                                Console.WriteLine($"网络错误，请检查网络状况或者代理设置...开始重试.....");
+                                Log.Info(nameof(Update_UpdateProgram),$"网络错误，请检查网络状况或者代理设置...开始重试.....");
                                 break;
                         }
                     }
                     catch (Exception ex)
                     {
                         error_count++;
-                        Console.WriteLine($"出现网络错误，错误详情：{ex.ToString()}\r\n\r\n===========下载执行重试，如果没同一个文件重复提示错误，则表示重试成功==============\r\n");
+                        Log.Info(nameof(Update_UpdateProgram),$"出现网络错误");
 
                     }
                 } while (string.IsNullOrEmpty(str));
-                Console.WriteLine($"下载成功...");
                 return str;
             }
 
@@ -414,6 +417,9 @@ namespace Core.Tools
                     }
                     try
                     {
+                        HttpClient _httpClient = new HttpClient();
+                        _httpClient.Timeout = new TimeSpan(0, 0, 10);
+                        _httpClient.DefaultRequestHeaders.Referrer = new Uri("https://update5.ddtv.pro");
                         using var response = _httpClient.GetAsync(FileDownloadAddress, HttpCompletionOption.ResponseHeadersRead).Result;
                         response.EnsureSuccessStatusCode();
                         using var output = new FileStream(outputPath, FileMode.Create);
@@ -427,18 +433,18 @@ namespace Core.Tools
                         switch (webex.Status)
                         {
                             case WebExceptionStatus.Timeout:
-                                Console.WriteLine($"下载文件超时:{FileDownloadAddress}");
+                                Log.Info(nameof(Update_UpdateProgram),$"下载文件超时:{FileDownloadAddress}");
                                 break;
 
                             default:
-                                Console.WriteLine($"网络错误，请检查网络状况或者代理设置...开始重试.....");
+                                Log.Info(nameof(Update_UpdateProgram),$"网络错误，请检查网络状况或者代理设置...开始重试.....");
                                 break;
                         }
                     }
                     catch (Exception ex)
                     {
                         error_count++;
-                        Console.WriteLine($"出现网络错误，错误详情：{ex.ToString()}\r\n\r\n===========执行重试，如果没同一个文件重复提示错误，则表示重试成功==============\r\n");
+                        Log.Info(nameof(Update_UpdateProgram),$"出现网络错误2");
 
                     }
                     Thread.Sleep(1000);
