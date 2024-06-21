953b05a9adf20a8a2a62f72f2359e86a686e7dc7
米 <kuanhzilin@gmail.com>
2024-06-22 03:56:40 +0800

    给Windows下update程序增加更新完成后自动启动对应客户端的功能

---
+++ Update/DisableConsoleQuickEdit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Update
+{
+    static class DisableConsoleQuickEdit
+    {
+#if WINDOWS
+        const uint ENABLE_QUICK_EDIT = 0x0040;  // STD_INPUT_HANDLE (DWORD): -10 is the standard input device.
+        const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+#endif
+        internal static bool Go()
+        {
+
+            if (!OperatingSystem.IsWindows())
+            {
+                return false;
+            }
+#if WINDOWS
+            IntPtr consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
+
+            // get current console mode
+            uint consoleMode;
+            if (!GetConsoleMode(consoleHandle, out consoleMode))
+            {
+                // ERROR: Unable to get console mode.
+                return false;
+            }
+
+            // Clear the quick edit bit in the mode flags
+            consoleMode &= ~ENABLE_QUICK_EDIT;
+
+            // set the new mode
+            if (!SetConsoleMode(consoleHandle, consoleMode))
+            {
+                // ERROR: Unable to set console mode
+                return false;
+            }
+#endif
+            return true;
+        }
+    }
+
+}
